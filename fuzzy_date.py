7fa633ddb8661bce3709ae56bba18e25ff619ec6
circleratio <tf@dsl.gr.jp>
2023-09-18 12:15:23 +0900

    [add] new patterns

--- fuzzy_date.py
+++ fuzzy_date.py
@@ -3,14 +3,14 @@ import re
 import datetime
 import calendar
 
-def fuzzy_datetime(arg):
+def fuzzy_date(arg):
     dt_now = datetime.date.today()
-    m = re.match('([^,]+), *([^,]+)', arg)
+    m = re.match(' *([^,]+), *([^,]+[^ ]) *', arg)
     if m:
-        dt_orig = fuzzy_datetime_parse(m.group(2), dt_now)
-        return(fuzzy_datetime_parse(m.group(1), dt_orig))
+        dt_orig = fuzzy_date_parse(m.group(2), dt_now)
+        return(fuzzy_date_parse(m.group(1), dt_orig))
     else:
-        return(fuzzy_datetime_parse(arg, dt_now))
+        return(fuzzy_date_parse(arg, dt_now))
     
 def conv_weekday(s):
     s = s.lower()
@@ -33,7 +33,7 @@ def conv_weekday(s):
     
     return(s)
     
-def fuzzy_datetime_parse(arg, dt_orig):
+def fuzzy_date_parse(arg, dt_orig):
     arg = conv_weekday(arg)
     
     m = re.match('([0-9]+)/([0-9]+)/([0-9]+)$', arg)
@@ -58,14 +58,12 @@ def fuzzy_datetime_parse(arg, dt_orig):
         dt = datetime.datetime(dt_orig.year, month, day)
         return(dt)
 
-    m = re.match('([Mm]o|[Tt]u|[Ww]e|[Tt]h|[Ff]r|[Ss]a|[Ss]u)$', arg)
+    m = re.match('(mo|tu|we|th|fr|sa|su)$', arg)
     if m:
         k = m.group(1).lower()
         pos = 'motuwethfrsasu'.find(k) // 2
         dow = dt_orig.weekday()
         day_diff = pos - dow
-        if day_diff <= 0:
-            day_diff += 7
         dt = dt_orig + datetime.timedelta(days=day_diff)
         return(dt)
 
@@ -81,6 +79,10 @@ def fuzzy_datetime_parse(arg, dt_orig):
     
     if arg == 'yesterday':
         dt = dt_orig + datetime.timedelta(days=-1)
+        return(dt)
+    
+    if arg == 'today':
+        dt = dt_orig + datetime.timedelta(days=0)
         return(dt)
     
     if arg == 'tomorrow':
@@ -107,6 +109,23 @@ def fuzzy_datetime_parse(arg, dt_orig):
         dt = dt_orig + datetime.timedelta(days=n)
         return(dt)
     
+    if arg == 'this week': # Monday of this week
+        dow = dt_orig.weekday()
+        dt = dt_orig + datetime.timedelta(days=-dow)
+        return(dt)
+        
+    if arg == 'next week': # Monday of next week
+        dow = dt_orig.weekday()
+        dt = dt_orig + datetime.timedelta(days=-dow+7)
+        return(dt)
+    
+    m = re.match('next (mo|tu|we|th|fr|sa|su)', arg)
+    if m:
+        dow = dt_orig.weekday()
+        pos = 'motuwethfrsasu'.find(m.group(1)) // 2
+        dt = dt_orig + datetime.timedelta(days=-dow+7+pos)
+        return(dt)
+    
     if arg == 'end of month':
         c = calendar.monthrange(dt_orig.year, dt_orig.month)
         dt = datetime.datetime(dt_orig.year, dt_orig.month, c[1])
@@ -116,7 +135,7 @@ def fuzzy_datetime_parse(arg, dt_orig):
     return(None)
         
 def main():
-    dt = fuzzy_datetime(sys.argv[1])
+    dt = fuzzy_date(sys.argv[1])
     print(dt)
 
 if __name__ == "__main__":
